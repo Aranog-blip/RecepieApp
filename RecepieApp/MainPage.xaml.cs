2d3037fc599f1b27a67b8caa0831597e738556ac
Aranog-Blip <nedimturkovic@hotmail.com>
2023-04-17 03:27:14 +0200

    Need to add a expanding list for the Recepie book

--- RecepieApp/MainPage.xaml.cs
+++ RecepieApp/MainPage.xaml.cs
@@ -1,13 +1,22 @@
-﻿namespace RecepieApp;
+﻿using System.IO;
+
+namespace RecepieApp;
 
 public partial class MainPage : ContentPage
 {
+	
 	
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+    private void OnClickSave (object sender, EventArgs e)
+	{ 
+	
+
+    }
+
 
 }
 
